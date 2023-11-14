397aad522dea265fbc7e5af0a74f43cad5d6f0d3
konnofuente <konomelifuente@gmail.com>
2023-11-14 11:44:55 +0100

    feat: Created IconWithLabel Components

---
+++ src/components/IconWithLabel/index.vue
@@ -0,0 +1,43 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+   <div class="icon-with-label">
+    <div
+    v-html="svgContent"
+    :class="iconSizeClass"
+    ></div>
+    <div class="label">
+        {{ labelText }}
+    </div>
+   </div>
+</template>
+
+<script>
+    export default {
+        name:'IconWithLabel',
+        props:{
+            svgContent:{
+                type:String,
+                require:true
+            },
+            labelText:{
+                type:String,
+                require:true
+            },
+            iconSize:{
+                type:String,
+                require:true
+            },
+
+        computed:{
+            iconSizeClass(){
+                return this.iconSize
+            }
+        }
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
