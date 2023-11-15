d01549bb8da6d6316db52033d79a55e0a6917dff
konnofuente <konomelifuente@gmail.com>
2023-11-15 11:06:40 +0100

    feat: Community section responsive

--- src/components/Community/index.vue
+++ src/components/Community/index.vue
@@ -1,10 +1,10 @@
 
 <template>
-    <div class="container mx-auto p-4 grid grid-cols-1 md:grid-cols-4 gap-4">
+    <div class="container mx-auto p-4 sm:grid grid-cols-1 md:grid-cols-4 gap-4">
       <!-- Sidebar: Sectors and Topics -->
-      <aside class="col-span-1">
+      <aside class="col-span-1 hidden sm:block">
         <!-- Sectors -->
-        <div class="bg-gray-200 p-4 rounded-lg">
+        <div class="bg-gray-200 p-4 rounded-lg px-10 py-5">
           <h2 class="font-bold mb-4">Sectors</h2>
           <ul>
             <li v-for="sector in sectors" :key="sector">
@@ -15,19 +15,10 @@
             </li>
           </ul>
         </div>
-        <!-- Recommended Topics -->
-        <div class="bg-gray-200 p-4 rounded-lg mt-4">
-          <h2 class="font-bold mb-4">Recommended topics</h2>
-          <div class="flex flex-wrap gap-2">
-            <span class="bg-blue-500 text-white px-2 py-1 rounded" v-for="topic in topics" :key="topic">
-              {{ topic }}
-            </span>
-          </div>
-        </div>
       </aside>
       
       <!-- Main Content Area: Posts -->
-      <main class="col-span-2">
+      <main class="col-span-2 shadow-sm">
           <!-- Post -->
           <article class="bg-white p-4 rounded-lg shadow-md">
               <!-- Post Header -->
@@ -66,21 +57,10 @@
       </main>
 
 
-      <aside class="col-span-1">
-              <!-- Sectors -->
-              <div class="bg-gray-200 p-4 rounded-lg">
-                <h2 class="font-bold mb-4">Sectors</h2>
-                <ul>
-                  <li v-for="sector in sectors" :key="sector">
-                    <label class="flex items-center space-x-2">
-                      <input type="checkbox" />
-                      <span>{{ sector }}</span>
-                    </label>
-                  </li>
-                </ul>
-              </div>
+      <aside class="col-span-1  hidden sm:block ">
+             
               <!-- Recommended Topics -->
-              <div class="bg-gray-200 p-4 rounded-lg mt-4">
+              <div class="bg-gray-200 p-4 rounded-lg ">
                 <h2 class="font-bold mb-4">Recommended topics</h2>
                 <div class="flex flex-wrap gap-2">
                   <span class="bg-blue-500 text-white px-2 py-1 rounded" v-for="topic in topics" :key="topic">
