44a10385d8512a9c2792eb68e4496b7fff50f517
konnofuente <konomelifuente@gmail.com>
2023-11-27 16:01:08 +0100

    feat: Profile-page-v1

--- src/features/SocialProfile/SocialProfile.vue
+++ src/features/SocialProfile/SocialProfile.vue
@@ -1,89 +1,138 @@
 <template>
   <div class="h-full">
     <div class="bg-white p-6">
-      <div class="flex flex-col items-center">
-        <!-- Logo/Profile Image -->
-        <img
-          src="public\images\unicef.png"
-          alt="Profile Image"
-          class="w-26 h-26 rounded-full mb-4"
-        />
+        <top-profile-info
+  :profileImageUrl="'public\\images\\unicef.png'"
+  profileName="Unicef International"
+  :followersCount="500"
+  :postsCount="34"
+  :isCurrentUser="true"
+/>
 
-        <!-- Profile Name -->
-        <h2 class="text-2xl font-semibold text-gray-800 mb-1">Profile name</h2>
-
-        <!-- Followers and Articles/Posts Info -->
-        <div class="flex space-x-3 mb-4">
-          <p><span> 500 </span>followers</p>
-          ·
-          <p><span> 500 </span>Articles/Posts</p>
-        </div>
-
-        <!-- Action Buttons -->
-        <div class="flex gap-4">
-          <button-ui
-            label="Edit Profile"
-            width="full"
-            leftIcon="src\assets\icons\edit.svg"
-            :color="'bg-primary-normal'"
-            :isRoundedMd="true"
-            :textCss="'text-white-normal font-bold'"
-          >
-          </button-ui>
-
-          <button-ui
-            label="Edit Profile"
-            width="full"
-            leftIcon="src\assets\icons\add-circle-white.svg"
-            :color="'bg-primary-normal'"
-            :isRoundedMd="true"
-            :textCss="'text-white-normal font-bold'"
-          >
-          </button-ui>
-        </div>
-      </div>
     </div>
 
-
     <div class="md:px-100 pb-5">
-        <div class="container mx-auto pt-3 sm:grid grid-cols-1 md:grid-cols-4 gap-10">
+      <div class="container  mx-auto pt-3 sm:grid grid-cols-1 md:grid-cols-4">
+        <!-- Sidebar: Sectors and Topics -->
+        <aside class="col-span-1   mb-2 sm:block">
+          <about-user-info
+            username="Unicef International"
+            description="The UN, through the United Nations Children's Fund UNICEF and specialized agencies such as the World Health Organization WHO."
+            location="From Yaounde, Cameroon"
+            phone="+320 236 259 6361"
+            email="unicef@gmail.org"
+            joinDate="10-03-2023"
+            website="unicef.org"
+          />
+        </aside>
 
-        </div>
+        <!-- Main Content Area: Posts -->
+        <main class="col-span-2 sm:px-4">
+          <div class="space-y-5">
+            <PostComponent
+              v-for="(post, index) in posts"
+              :key="index"
+              :username="post.username"
+              :postDate="post.postDate"
+              :postTitle="post.postTitle"
+              :postContent="post.postContent"
+              :userProfileImage="post.userProfileImage"
+              :listLikers="post.listLikers"
+              :comments="post.comments"
+              :postImages="post.postImages"
+            />
+          </div>
+        </main>
+
+        <aside class="col-span-1 hidden sm:block">
+        
+        </aside>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import ButtonUi from '../../components/base/ButtonUi.vue'
-// import PostComponent from '../../components//'
+import AboutUserInfo from './components/AboutUserInfo/index.vue'
+import TopProfileInfo from './components/TopProfileInfo/index.vue'
+import PostComponent from '../../components/common/post/index.vue'
 export default {
   name: 'SocialProfile',
 
   components: {
-    ButtonUi
+    PostComponent,
+    AboutUserInfo,
+    TopProfileInfo
+  },
+
+  data() {
+    return {
+      posts: [
+        {
+          username: 'User-name',
+          postDate: 'Posted: 20 October',
+          postTitle: "Heavy rains, landslide in Cameroon's west kill at least 34",
+          postContent:
+            "Introduction UNICEF, the United Nations International Children's Emergency Fund, is a globally recognized organization dedicated to...",
+          userProfileImage:
+            'https://th.bing.com/th/id/R.007eff93a66b6a7de41ee3e7ca553e92?rik=kiX%2fqnsUx6REXw&pid=ImgRaw&r=0',
+          listLikers: Array(22).fill('national embassy'),
+          comments: Array(22).fill('national embassy'),
+          postImages: [
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 1'
+            },
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 2'
+            },
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 3'
+            },
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 4'
+            }
+          ]
+        },
+        {
+          username: 'User-name',
+          postDate: 'Posted: 20 October',
+          postTitle: "Heavy rains, landslide in Cameroon's west kill at least 34",
+          postContent:
+            "Introduction UNICEF, the United Nations International Children's Emergency Fund, is a globally recognized organization dedicated to...",
+          userProfileImage:
+            'https://th.bing.com/th/id/R.007eff93a66b6a7de41ee3e7ca553e92?rik=kiX%2fqnsUx6REXw&pid=ImgRaw&r=0',
+          listLikers: Array(22).fill('national embassy'),
+          comments: Array(22).fill('national embassy'),
+          postImages: [
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 1'
+            },
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 2'
+            },
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 3'
+            },
+            {
+              src: 'https://th.bing.com/th/id/OIP.bxjnCc07ZANhF94zX2MjyQHaE6?rs=1&pid=ImgDetMain',
+              alt: 'Description of image 4'
+            }
+          ]
+        }
+        // ... more post data
+      ]
+    }
   }
 }
 </script>
 
 <style scoped>
-span {
-  color: var(--gray-darker, #252525);
-  text-align: center;
-  font-family: Raleway;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 24px; /* 150% */
-}
 
-p {
-  color: var(--gray-dark, #505050);
-
-  /* Paragraphs/P2 */
-  font-family: Raleway;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 24px;
-}
 </style>
---
+++ src/features/SocialProfile/components/TopProfileInfo/index.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="flex flex-col items-center">
+      <!-- Logo/Profile Image -->
+      <img :src="profileImageUrl" alt="Profile Image" class="w-26 h-26 rounded-full mb-4" />
+  
+      <!-- Profile Name -->
+      <h2 class="text-2xl font-semibold text-gray-800 mb-1">{{ profileName }}</h2>
+  
+      <!-- Followers and Articles/Posts Info -->
+      <div class="flex space-x-3 mb-4">
+        <p><span>{{ followersCount }}</span> followers</p>
+        ·   
+        <p><span>{{ postsCount }}</span> Articles/Posts</p>
+      </div>
+  
+      <!-- Action Buttons -->
+      <div v-if="false" class="flex space-x-2">
+        <button-ui
+          label="Edit Profile"
+          leftIcon="src/assets/icons/edit.svg"
+          color="bg-primary-normal"
+          isRoundedMd="true"
+          textCss="text-white-normal font-bold"
+          @clickButton="editProfile"
+        />
+  
+        <button-ui
+          label="Create Post"
+          leftIcon="src/assets/icons/add-circle-white.svg"
+          color="bg-primary-normal"
+          isRoundedMd="true"
+          textCss="text-white-normal font-bold"
+          @clickButton="createPost"
+        />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import ButtonUi from '../../../../components/base/ButtonUi.vue'
+  
+  export default {
+    name: 'TopProfileInfo',
+    components: {
+      ButtonUi
+    },
+    props: {
+      profileImageUrl: {
+        type: String,
+        required: true
+      },
+      profileName: {
+        type: String,
+        required: true
+      },
+      followersCount: {
+        type: [Number, String],
+        required: true
+      },
+      postsCount: {
+        type: [Number, String],
+        required: true
+      },
+      isCurrentUser: {
+        type: Boolean,
+        default: true
+      }
+    },
+    methods: {
+      editProfile() {
+        // Trigger edit profile action
+      },
+      createPost() {
+        // Trigger create post action
+      }
+    }
+  }
+  </script>
+  
+  <style lang="scss" scoped>
+    /* Add your styles here if necessary */
+    span {
+  color: var(--gray-darker, #252525);
+  text-align: center;
+  font-family: Raleway;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px; /* 150% */
+}
+
+p {
+  color: var(--gray-dark, #505050);
+
+  /* Paragraphs/P2 */
+  font-family: Raleway;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+}
+
+  </style>
+  
