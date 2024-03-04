69a81fe583b5a55049ee4edd8df328d5585b0e48
konnofuente <95084135+konnofuente@users.noreply.github.com>
2024-03-04 02:27:46 +0100

    feat(app): extract keys from svg

--- src/features/DashBaord/DashBoardView.vue
+++ src/features/DashBaord/DashBoardView.vue
@@ -2,10 +2,17 @@
   <div class="bg-primary-light px-100 pt-10">
     <div class="grid grid-cols-1 md:grid-cols-8 gap-2">
       <div class="col-span-1">
-
+<!-- 
         <div v-for="(key, index) in vectorKeys" :key="index">
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-              :style="{ backgroundColor: key.color }">@{{ key.id }}</span>
+              :style="{ backgroundColor: key.color }">{{ key.name }}</span>
+      </div> -->
+
+      <div class="flex flex-wrap gap-2 p-4">
+        <div v-for="(key, index) in vectorKeys" :key="index" class="flex items-center gap-2">
+          <span class="block w-4 h-4 rounded-full" :style="{ backgroundColor: key.color }"></span>
+          <span class="text-sm font-semibold" :class="{'text-gray-700': !key.color, 'text-primary-normal': key.color}" >{{ key.name }}</span>
+        </div>
       </div>
       </div>
     
@@ -70,7 +77,9 @@ import InlineSvg from 'vue-inline-svg'
 
 export default {
   name: 'DashBoardView',
-  mounted() {},
+  mounted() {
+    this.extractSVGKeys()
+  },
   data() {
     return {
     
@@ -125,42 +134,35 @@ export default {
     }
   },
 
-  methods: {
-    processSVGFile(event) {
-      this.vectorKeys.splice(0, this.vectorKeys.length)
-
-      const file = event.target.files[0]
-      if (!file) {
-        this.imageFile = null
-        return
-      }
-
-      this.isSvg = file.type === 'image/svg+xml'
-      this.imageFile = URL.createObjectURL(file)
-      const reader = new FileReader()
-      reader.onload = (e) => {
-        const parser = new DOMParser()
-        const svgDoc = parser.parseFromString(e.target.result, 'image/svg+xml')
-        const paths = svgDoc.querySelectorAll('path')
-
-        const extractedData = Array.from(paths).map((path) => ({
-          id: path.getAttribute('data-id'),
-          value: this.extractColor(path.getAttribute('style')),
-          type: this.isSvg ? 'color' : 'image',
-          name: path.getAttribute('data-name'),
-          color: this.extractColor(path.getAttribute('style'))
-        }))
-
-        this.vectorKeys.push(...extractedData)
-      }
-      reader.readAsText(file)
-    },
-
-    extractColor(styleString) {
-      const match = styleString.match(/fill: (\\#[0-9a-fA-F]{6})/)
-      return match ? match[1] : 'DefaultColor' // Return a default color or null if no match
-    }
-  },
+ methods: {
+   async extractSVGKeys() {
+     this.vectorKeys.splice(0, this.vectorKeys.length)
+ 
+     const response = await fetch(this.mapSvgPath)
+     const svgText = await response.text()
+ 
+     const parser = new DOMParser()
+     const svgDoc = parser.parseFromString(svgText, 'image/svg+xml')
+     const paths = svgDoc.querySelectorAll('path')
+ 
+     const extractedData = Array.from(paths).map((path) => ({
+       id: path.getAttribute('data-id'),
+       value: this.extractColor(path.getAttribute('style')),
+       type: this.isSvg ? 'color' : 'image',
+       name: path.getAttribute('data-name'),
+       color: this.extractColor(path.getAttribute('style'))
+     }))
+ 
+     this.vectorKeys.push(...extractedData)
+     console.log(this.vectorKeys)
+   },
+ 
+   extractColor(styleString) {
+     const match = styleString.match(/fill: (#[0-9a-fA-F]{6})/)
+     return match ? match[1] : 'DefaultColor'
+   }
+ }
+,
 
   computed: {
     isSVG() {
