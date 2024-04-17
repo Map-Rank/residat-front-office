2527f3f5a3d2eb6ea2ecb324e3c03e17438ddbe3
konnofuente <konomelifuente@gmail.com>
2024-04-17 15:09:42 +0100

    refactor(app): display of 3d map

--- src/features/DashBaord/DashBoardView.vue
+++ src/features/DashBaord/DashBoardView.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="bg-primary-light px-4 md:px-[50px] pt-1 w-full min-h-screen">
+    <div class=" bg-white-normal h-10">
+      <div class="h-full  bg-white flex  items-center px-4 space-x-4" >
+        <img  src="\assets\icons\back-arrow.png"  @click="goBack"  class="h-8" alt=""  />
+        <p>Previous Map</p>
+      </div>
+
+    </div>
+
     <div
       class="grid mt-4 space-y-4 md:space-y-0 md:flex md:space-x-4 row-auto md:justify-between md:h-10 z-1 relative"
     >
@@ -101,8 +109,29 @@
       </div>
     </div>
 
+
+
+<div class="bg-white h-[150px] rounded-lg">
+  <div class="hidden lg:flex justify-between p-4">
+    <div class="border border-gray-200 rounded-lg overflow-hidden shadow-md">
+      <img src="\assets\images\DashBoard\3d-map.jpg" alt="Image 1" class="w-full h-[120px] object-cover" />
+    </div>
+    <div class="border border-gray-200 rounded-lg overflow-hidden shadow-md">
+      <img src="\assets\images\DashBoard\3d-map.jpg" alt="Image 2" class="w-full h-[120px] object-cover" />
+    </div>
+    <div class="border border-gray-200 rounded-lg overflow-hidden shadow-md">
+      <img src="\assets\images\DashBoard\3d-map.jpg" alt="Image 3" class="w-full h-[120px] object-cover" />
+    </div>
+    <div class="border border-gray-200 rounded-lg overflow-hidden shadow-md">
+      <img src="\assets\images\DashBoard\3d-map.jpg" alt="Image 4" class="w-full h-[120px] object-cover" />
+    </div>
+  </div>
+</div>
+
+
+
     <div
-      class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 py-10 space-y-2 space-x-3"
+      class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 py-10  md:space-x-3"
       :class="{ hidden: !displayStatistics }"
     >
       <div class="col-span-1">
@@ -326,6 +355,11 @@ export default {
           })
         }
       }
+    },
+
+    goBack() {
+      // Use router.go(-1) to navigate back
+      this.$router.go(-1);
     },
 
     reloadMap() {},
