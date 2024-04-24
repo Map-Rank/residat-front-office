6aedd1ae049c832d532bbf680fba768ee88e93c8
konnofuente <konomelifuente@gmail.com>
2024-04-24 12:25:57 +0100

    feat(app): sideBar

--- src/features/Setting/SettingView.vue
+++ src/features/Setting/SettingView.vue
@@ -1,16 +1,30 @@
 <template>
     <div class="flex h-screen bg-primary-light">
       <!-- Sidebar -->
-      <div class="w-64 bg-white hidden sm:block">
+      <div class="w-2/8 bg-white hidden sm:block">
         <div class="flex flex-col h-full">
-          <!-- Sidebar content here -->
-          <!-- Add more sidebar items as needed -->
-          <SideBarItem title="Account preferences"></SideBarItem>
-          <SideBarItem title="Sign in & security"></SideBarItem>
-          <SideBarItem title="Visibility"></SideBarItem>
-          <SideBarItem title="Data privacy"></SideBarItem>
-          <SideBarItem title="Advertising data"></SideBarItem>
-          <SideBarItem title="Notifications"></SideBarItem>
+          <div class="flex p-5 space-x-4 items-center " >
+            <img src="\assets\images\Community\profile.png" class="h-[40px]" />
+            <h2>Settings</h2>
+          </div>
+
+          <div class="p-5 space-y-5 ">
+
+            <IconwithLabel
+            v-for="(item, index) in navItems"
+            :svgContentHover="item.svgContentHover"
+            :svgContent="item.svgContent"
+            :labelText="item.labelText"
+            :labelTextRight="item.labelText"
+            :textCss="'text-primary-normal text-lg ml-5'"
+            :iconDesktopSize="this.iconSize"
+            :isActive="item.isActive"
+            :right="item.right"
+           
+           
+            :key="index"
+          ></IconwithLabel>
+          </div>
         </div>
       </div>
       <!-- Settings content -->
@@ -30,18 +44,31 @@
   </template>
   
   <script>
-import SideBarItem from './components/SideBarItem.vue';
+// import SideBarItem from './components/SideBarItem.vue';
 import SettingsSection from '@/features/Setting/components/SettingsSection.vue'
+import IconwithLabel from '@/components/common/IconWithLabel/index.vue'
 
   export default {
     name:'SettingView',
     components: {
-        SideBarItem,
-        SettingsSection
+        // SideBarItem,
+        SettingsSection,
+        IconwithLabel
 
+    },
+
+    methods:{
+      isActive(routerName) {
+      if (this.$route.name === routerName) {
+        return true
+      } else {
+        return false
+      }
+    },
     },
     data() {
     return {
+      iconSize: 'w-[30px] h-[30px]',
       profileItems: [
         { name: 'Name, location and enterprise', route: '/name-location' },
         { name: 'Center of interest', route: '/center-interest' }
@@ -49,6 +76,24 @@ import SettingsSection from '@/features/Setting/components/SettingsSection.vue'
       generalItems: [
         { name: 'Language', route: '/language' },
         { name: 'Close Account', route: '/close-account' }
+      ],
+      navItems: [
+        {
+          svgContent: '\\assets\\icons\\profile-outline.svg',
+          svgContentHover: '\\assets\\icons\\profile-fill.svg',
+          labelText: 'Account preferences',
+          isActive: false,
+          right: true,
+          routerName: 'community'
+        },
+        {
+          svgContent: '\\assets\\icons\\dashboard-outline.svg',
+          svgContentHover: '\\assets\\icons\\dashboard-fill.svg',
+          labelText: 'Sign In & Security',
+          isActive: false,
+          right: true,
+          routerName: 'dashbaord'
+        },
       ]
     };
   }
--- src/features/Setting/components/SideBarItem.vue
+++ src/features/Setting/components/SideBarItem.vue
@@ -1,13 +1,47 @@
+import IconWithLabel from '@/components/common/IconWithLabel/index.vue';
 <template>
-    <div class="px-4 py-2 hover:bg-gray-100 cursor-pointer">
-      <span class="text-gray-700 text-sm font-medium">{{ title }}</span>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'SidebarItem',
-    props: ['title']
-  }
-  </script>
-  
+  <div
+    class="flex items-center p-2 my-1 text-gray-700 bg-white rounded-lg shadow cursor-pointer hover:bg-gray-100"
+    :class="{ 'bg-gray-200': isActive }"
+    @click="handleClick"
+  >
+
+    <!-- <span class="ml-2 text-sm font-medium">{{ title }}</span> -->
+    <IconwithLabel
+    
+    >
+
+    </IconwithLabel>
+
+  </div>
+</template>
+
+<script>
+import IconwithLabel from '@/components/common/IconWithLabel/index.vue'
+
+export default {
+  name: 'SidebarItem',
+  props: {
+    title: String,
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+    // Add any other props here, such as routing information if needed
+  },
+  components:{
+    IconwithLabel
+  },
+  methods: {
+    handleClick() {
+      // Emit an event when the item is clicked
+      this.$emit('clicked', this.title);
+      // Add your own logic to handle the click event, like routing
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Scoped styles if needed */
+</style>
