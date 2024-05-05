ee8e1f6e2e2ce455889e76758815d36c1d645bd1
konnofuente <konomelifuente@gmail.com>
2024-05-05 03:09:32 +0100

    feat(app): translated auth screens

--- src/main.js
+++ src/main.js
@@ -10,15 +10,28 @@ import InlineSvg from 'vue-inline-svg';
 // import useEventStore from './stores/eventStore.js'
 import App from './App.vue'
 import router from './router'
+import en from '@/langs/en.json'
+import fr from '@/langs/fr.json'
+import { createI18n } from 'vue-i18n';
+
+const messages = {
+    en: en,
+    fr: fr
+};
+const i18n = createI18n({
+    locale: 'en', 
+    fallbackLocale: 'en', 
+    messages 
+});
 
 const app = createApp(App)
 
 // Register InlineSvg globally
 app.component('inline-svg', InlineSvg);
-
 app.use(createPinia())
 app.use(router)
 app.use(VeeValidatePlugin)
+app.use(i18n);
 
 const sectorStore = useSectorStore()
 sectorStore.initializeStore()
