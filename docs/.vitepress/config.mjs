3c4fe4242c1f94343129769c32b9a2748bbd321a
konnofuente <konomelifuente@gmail.com>
2024-05-23 20:53:24 +0100

    update readme

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/residat-front-office/',
+  base: '/residat-front-office',
   title: "Map and Rank Doc",
   description: "This is site is aim in presenting map and rank doc",
   themeConfig: {
