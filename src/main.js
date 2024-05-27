d9bb55426f630b240e836b522e27a29c66decdd8
konnofuente <konomelifuente@gmail.com>
2024-05-27 16:13:22 +0100

    ci(app): #144 update main.js

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import InlineSvg from 'vue-inline-svg'
 import App from './App.vue'
 import router from './router'
 import { i18n } from '@/langs/i18nSetup'
+import Toast, { POSITION } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import { app as firebaseApp, analytics } from './firebaseConfig'; 
 
 const app = createApp(App)
@@ -22,6 +24,24 @@ app.use(router)
 app.use(VeeValidatePlugin)
 app.use(i18n)
 
+
+const options = {
+    position: POSITION.TOP_CENTER,
+    timeout: 3000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+};
+
+app.use(Toast, options);
+
 const sectorStore = useSectorStore()
 sectorStore.initializeStore()
 
@@ -29,4 +49,3 @@ const authStore = useAuthStore()
 authStore.initializeAuthState()
 
 app.mount('#app')
-
