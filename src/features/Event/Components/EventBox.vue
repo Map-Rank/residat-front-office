3261ce2d2a22b2e1c0a4667484b65552049823ad
konnofuente <konomelifuente@gmail.com>
2024-07-01 14:22:54 +0100

    refactor(app): #164 event details flyers

--- src/features/Event/Components/EventBox.vue
+++ src/features/Event/Components/EventBox.vue
@@ -1,12 +1,18 @@
 <template>
   <div 
-  @click="navigateEventDetail"
-  class="mb-4 p-4 bg-white rounded cursor-pointer">
-    <div to="/event-detail/1" class="grid sm:flex mb-2">
-      <img :src="event.image" class="rounded-[50%] h-[100px] w-[100px] mr-4" alt="event image" />
+  
+  class="mb-4 p-4 bg-white rounded ">
+    <div 
+   
+    to="/event-detail/1" class="grid sm:flex mb-2">
+      <img 
+       @click="navigateEventDetail"
+      :src="event.image" class="cursor-pointer rounded-[50%] h-[100px] w-[100px] mr-4" alt="event image" />
       <div class="w-full">
         <div class="flex justify-between ">
-          <h3 class="text-xl text-start">{{ event.title }}</h3>
+          <h3
+           @click="navigateEventDetail"
+          class="text-xl text-start cursor-pointer">{{ event.title }}</h3>
 
           <!-- Dropdown menu -->
           <div v-if="showMenu" class="menu relative">
@@ -17,6 +23,8 @@
                 />
               </svg>
             </button>
+
+
             <div v-show="isMenuVisible" class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50">
               <button-ui
                 :label="'Edit'"
@@ -32,9 +40,13 @@
               ></button-ui>
             </div>
           </div>
+
+
         </div>
         <div>
-          <div class="grid items-center">
+          <div 
+           @click="navigateEventDetail"
+          class="grid items-center cursor-pointer">
             <div class="flex items-center">
               <img src="/assets/icons/location.png" class="h-4 mr-1" alt="location icon" />
               <span class="span">{{ event.location }} • </span>
--- src/features/Event/Pages/EventDetails.vue
+++ src/features/Event/Pages/EventDetails.vue
@@ -4,14 +4,15 @@
 
     <div v-if="!isLoading">
       <!-- Event Banner -->
-      <div class="relative mb-6">
+      <div class=" hidden relative mb-6">
         <img
           :src="event.image"
           alt="Event Banner"
-          class="w-full h-64 object-cover rounded-lg shadow-lg"
+          class="w-full h-64 object-cover rounded-lg shadow-lg cursor-pointer"
+          @click="showImageModal(event.image)"
         />
-        <div class="absolute top-0 left-0 bg-black bg-opacity-70 text-white p-4 rounded-t-lg">
-          <h1 class="text-3xl font-bold">{{ event.title }}</h1>
+        <div class="hidden md:block absolute top-0 left-0 bg-black bg-opacity-70 text-white p-2 rounded-t-lg">
+          <h1 class=" text-[24px] font-bold">{{ event.title }}</h1>
           <p class="text-sm text-white">{{ event.humanize_date_creation }}</p>
         </div>
       </div>
@@ -19,39 +20,20 @@
       <!-- Event Details -->
       <div class="bg-white p-6 rounded-lg shadow-md">
         <div class="flex justify-between items-center mb-4">
-          <h2 class="text-2xl font-semibold">{{ event.title }}</h2>
-          <button v-if="showMenu" @click="toggleMenu" class="p-2">
-            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-              <path
-                d="M5 12a2 2 0 110-4 2 2 0 010 4zm7 0a2 2 0 110-4 2 2 0 010 4zm7 0a2 2 0 110-4 2 2 0 010 4z"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div
-          v-if="isMenuVisible"
-          class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-50"
-        >
-          <button-ui
-            :label="'Edit'"
-            :textCss="'text-left'"
-            :customCss="'items-left justify-start hover:bg-gray-100'"
-            @clickButton="editEvent"
-          />
-          <button-ui
-            :label="'Delete'"
-            :textCss="'text-left'"
-            :customCss="'items-left justify-start hover:bg-gray-100'"
-            @clickButton="deleteEvent"
-          />
+          <h2 class="md:text-[20px] sm:text-[18px] text-[18px]  font-semibold">{{ event.title }}</h2>
         </div>
 
         <div class="flex mb-4">
-          <!-- <img :src="event.image" alt="Event Image" class="w-32 h-32 rounded-full mr-4" /> -->
           <div class="flex flex-col justify-center">
-            <p class="text-lg font-semibold"> By: {{ event.organized_by }}</p>
-            <p class="text-sm text-gray-600"> <span class="text-[14px] text-body-darker font-semibold" >Town : </span>{{ event.location }}</p>
-            <p class="text-sm text-gray-600"><span class="text-[14px] text-body-darker font-semibold" >Starting on : </span>{{ event.published_at }}</p>
+            <p class="text-lg font-semibold">By: {{ event.organized_by }}</p>
+            <p class="text-sm text-gray-600">
+              <span class="text-[14px] text-body-darker font-semibold">Town : </span
+              >{{ event.location }}
+            </p>
+            <p class="text-sm text-gray-600">
+              <span class="text-[14px] text-body-darker font-semibold">Date : </span
+              >{{ event.published_at }}
+            </p>
           </div>
         </div>
         <h3>Details</h3>
@@ -61,13 +43,26 @@
             {{ isReadMore ? 'Read Less' : 'Read More' }}
           </button>
         </p>
+        <h3 class="my-4">Event Flyers</h3>
+        <div class="h-[300px] overflow-x-auto flex space-x-4 bg-white border border-gray-300 rounded">
+          <img :src="event.image" alt="" srcset="" class="h-full object-cover cursor-pointer" @click="showImageModal(event.image)">
+      </div>
+      </div>
+    </div>
+
+    <!-- Image Modal -->
+    <div v-if="isImageModalVisible" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" @click="closeImageModal">
+      <div class="relative bg-white p-4 rounded shadow-lg max-h-[80vh] max-w-[90vw]" @click.stop>
+        <button @click="closeImageModal" class="absolute top-2 right-2 text-[20px] text-red-700 hover:text-gray-800">
+          &times;
+        </button>
+        <img :src="imageToPreview" alt="Image Preview" class="max-h-full w-auto rounded">
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ButtonUi from '@/components/base/ButtonUi.vue'
 import { deleteEvent } from '@/services/eventService'
 import { getSpecificEvent } from '@/services/eventService.js'
 import AvatarEventShimmer from '@/components/common/ShimmerLoading/AvatarPostShimmer.vue'
@@ -88,7 +83,7 @@ export default {
           console.error('An error occurred while fetching the event:', error)
           this.isLoading = true
         } finally {
-        //   console.log('Execution completed.')
+          //   console.log('Execution completed.')
         }
       }
     }
@@ -99,16 +94,17 @@ export default {
     eventId: String
   },
   components: {
-    ButtonUi,
     AvatarEventShimmer
   },
   data() {
     return {
-      isMenuVisible: false,
+      isMenuVisible: true,
       isReadMore: false,
       isLoading: false,
       descriptionLimit: 100,
-      event: {}
+      event: {},
+      isImageModalVisible: false,
+      imageToPreview: ''
     }
   },
   computed: {
@@ -143,6 +139,13 @@ export default {
     },
     toggleReadMore() {
       this.isReadMore = !this.isReadMore
+    },
+    showImageModal(imageUrl) {
+      this.imageToPreview = imageUrl
+      this.isImageModalVisible = true
+    },
+    closeImageModal() {
+      this.isImageModalVisible = false
     }
   }
 }
@@ -150,7 +153,4 @@ export default {
 
 <style scoped>
 /* Add your custom styles here */
-span{
-
-}
 </style>
